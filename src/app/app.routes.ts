1b42f2c35e7a05691791cdfd3834309671b85f80
BadgujarHrushikesh <hrushikeshbadgujar2002@gmail.com>
2024-06-15 20:54:02 +0530

    Navabr and Login Component add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,65 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { PagesComponent } from './pages/pages.component';
+
+
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthComponent,
+        children: [{
+            path: 'login',
+            component: LoginComponent
+        },
+        {
+            path: 'signup',
+            component: SignupComponent
+        },
+        {
+            path: 'forget-password',
+            component: SignupComponent
+        },
+        {
+            path: '**',
+            redirectTo: 'login'
+        }]
+    },
+    {
+        path: 'app',
+        component: PagesComponent,
+        children: [{
+            path: 'home',
+            component: HomeComponent
+        },
+        {
+            path: 'about-us',
+            component:  AboutComponent
+        },
+        {
+            path: 'contact-us',
+            component: ContactComponent
+        },
+        {
+            path: 'jobs',
+            component:  JobsComponent
+        },
+        {
+            path: '**',
+            redirectTo: 'home'
+        }]
+    },{
+        path: '**',
+        redirectTo: 'app'
+    }
+
+
+
+];
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+// public isLogin : Boolean = false;
+public isLogin : Boolean = true;
+
+
+}
